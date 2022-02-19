2835fa6654a79a89857c2111379e303b28871426
Zheyu Shen <arsdragonfly@gmail.com>
2022-02-19 16:14:27 +0800

    init with three.js and react-three-fiber

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# Reciv
+
+Reciv is a modern spiritual successor to Freeciv.
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app), using the [Redux](https://redux.js.org/) and [Redux Toolkit](https://redux-toolkit.js.org/) template.
 
 ## Available Scripts
--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import { Canvas } from '@react-three/fiber'
+import { Box } from './Box';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Canvas>
+          <ambientLight />
+          <pointLight position={[10, 10, 10]} />
+          <Box position={[-1.2, 0, 0]} />
+          <Box position={[1.2, 0, 0]} />
+        </Canvas>
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
         <p>
