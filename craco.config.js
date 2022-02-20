c953f519207973fd49b415bd32520707158a8a4f
Zheyu Shen <arsdragonfly@gmail.com>
2022-02-20 13:20:22 +0800

    craco and purescript

--- craco.config.js
+++ craco.config.js
@@ -1,2 +1,13 @@
+
 module.exports = {
-};
+    plugins: [
+        {
+            plugin: require('./craco-purescript-loader.config'),
+            options: {
+                spago: true,
+                watch: true,
+                pscIde: false,
+            },
+        },
+    ],
+}
